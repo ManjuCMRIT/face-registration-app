7072d7cb1b00dc763b0f2d397dd07858e4633f9f
ManjuCMRIT <121920349+ManjuCMRIT@users.noreply.github.com>
2025-12-26 22:53:34 +0530

    Enhance Firebase initialization and comments
    Updated Firebase initialization to include storage bucket configuration and improved comments.

--- firebase_utils.py
+++ firebase_utils.py
@@ -1,14 +1,20 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
 import json
 import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
 
-# Initialize Firebase ONLY ONCE
+# Initialize Firebase (run only once)
 if not firebase_admin._apps:
     cred = credentials.Certificate(
         json.loads(st.secrets["FIREBASE_KEY"])
     )
-    firebase_admin.initialize_app(cred)
+    firebase_admin.initialize_app(
+        cred,
+        {
+            "storageBucket": "face-registration-app.firebasestorage.app"
+        }
+    )
 
-# Firestore client
+# Firestore and Storage clients
 db = firestore.client()
+bucket = storage.bucket()
