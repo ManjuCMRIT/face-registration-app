c5f5d24b3ff9f05baef6c910a42e7d1148160ac3
ManjuCMRIT <121920349+ManjuCMRIT@users.noreply.github.com>
2025-12-26 22:51:13 +0530

    Update firebase_utils.py

--- firebase_utils.py
+++ firebase_utils.py
@@ -1,18 +1,14 @@
 import firebase_admin
-from firebase_admin import credentials, firestore, storage
+from firebase_admin import credentials, firestore
 import json
 import streamlit as st
 
+# Initialize Firebase ONLY ONCE
 if not firebase_admin._apps:
     cred = credentials.Certificate(
         json.loads(st.secrets["FIREBASE_KEY"])
     )
-    firebase_admin.initialize_app(
-        cred,
-        {
-            "storageBucket": "face-registration-app.firebasestorage.app"
-        }
-    )
+    firebase_admin.initialize_app(cred)
 
+# Firestore client
 db = firestore.client()
-bucket = storage.bucket()
